2ca6b399762affc8900168017abf83b860210cd5
Patrick Di Justo <patrick@dijusto.com>
2018-06-24 12:32:27 -0400

    Update MaintUpdate.sh
    Small fixes mean a lot

--- MaintUpdate.sh
+++ MaintUpdate.sh
@@ -2,17 +2,7 @@
 
 cd Watergate45
 
-echo 'Deleting Wgatetimeline' >> log.txt
-
-rm -rf wgatetimeline.csv
-
-echo 'Getting wgatetimeline' >> log.txt
-
-wget https://www.dropbox.com/s/ye6duakbjkqfmzs/wgatetimeline.csv?dl=1 -O wgatetimeline.csv
-
-echo 'Starting Friend List analysis' >> log.txt
-
-python WatergateMaintenance.py
+python LiarMaintenance.py
 
 echo 'done' >> log.txt
 
